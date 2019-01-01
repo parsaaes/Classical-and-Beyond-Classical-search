972cb84c365d19a7f045dc063d50c6ca4a113b25
parsaaes <parsaaes@gmail.com>
2019-01-01 13:49:16 +0330

    Complete uniform cost details

--- graph_uniform_cost.py
+++ graph_uniform_cost.py
@@ -8,6 +8,10 @@ from romania_problem import RomaniaProblem
 import problem_generator as pg
 
 def UCS(problem):
+    visited = 0
+    expanded_nodes = 0
+    maximum_memory = 0
+
     node = Node(problem.initial_state,None,None)
     frontier = list()
     heapq.heapify(frontier)
@@ -17,11 +21,13 @@ def UCS(problem):
     explored = set()
     while True:
        if len(frontier) == 0:
-           return None
+           return (None,None)
        node = heapq.heappop(frontier)
+       visited += 1
+
        print("POPED: " + node.state)
        if problem.goal_test(node.state):
-           return node
+           return (node,(visited,expanded_nodes,maximum_memory,node.cost))
        explored.add(node.state)
        for action in problem.actions(node.state):
            g = node.cost + problem.path_cost(node.state,problem.transition(node.state,action))
@@ -29,6 +35,8 @@ def UCS(problem):
            if (child.state not in explored and child.state not in frontier_state):
                 print(child.state + " <- " + child.parent.state + " : " + str(child.cost))
                 heapq.heappush(frontier,child)
+                expanded_nodes += 1
+                maximum_memory = max(maximum_memory, len(frontier) + len(explored))
                 frontier_state.append(child.state)
            elif child.state in frontier_state:
                for n in frontier:
@@ -42,9 +50,10 @@ def UCS(problem):
 def main():
     romania_graph = pg.get_romania_graph()
     romania_prob = RomaniaProblem("Arad","Bucharest", romania_graph)
-    node = UCS(romania_prob)
+    node, details = UCS(romania_prob)
     if node is not None:
         node.print_path()
+        print(details)
     return
 if __name__ == "__main__":
         main()
--- tree_uniform_cost.py
+++ tree_uniform_cost.py
@@ -8,6 +8,10 @@ from romania_problem import RomaniaProblem
 import problem_generator as pg
 
 def UCS(problem):
+    visited = 0
+    expanded_nodes = 0
+    maximum_memory = 0
+
     node = Node(problem.initial_state,None,None)
     frontier = list()
     heapq.heapify(frontier)
@@ -16,17 +20,21 @@ def UCS(problem):
     frontier_state.append(node.state)
     while True:
        if len(frontier) == 0:
-           return None
+           return (None,None)
        node = heapq.heappop(frontier)
+       visited += 1
+
        print("POPED: " + node.state)
        if problem.goal_test(node.state):
-           return node
+           return (node,(visited,expanded_nodes,maximum_memory,node.cost))
        for action in problem.actions(node.state):
            g = node.cost + problem.path_cost(node.state,problem.transition(node.state,action))
            child = node.child_node(problem, action, g)
            if (child.state not in frontier_state):
                 print(child.state + " <- " + child.parent.state + " : " + str(child.cost))
                 heapq.heappush(frontier,child)
+                expanded_nodes += 1
+                maximum_memory = max(maximum_memory, len(frontier))
                 frontier_state.append(child.state)
            elif child.state in frontier_state:
                for n in frontier:
@@ -40,9 +48,10 @@ def UCS(problem):
 def main():
     romania_graph = pg.get_romania_graph()
     romania_prob = RomaniaProblem("Arad","Bucharest", romania_graph)
-    node = UCS(romania_prob)
+    node, details = UCS(romania_prob)
     if node is not None:
         node.print_path()
+        print(details)
     return
 if __name__ == "__main__":
         main()
