5552615d78a8ebef801c7393dfa7d73bb93d3417
parsaaes <parsaaes@gmail.com>
2019-01-01 12:17:53 +0330

    Add details for BFS

--- graph_bfs.py
+++ graph_bfs.py
@@ -9,10 +9,14 @@ def child_node(problem, node, action):
 
 
 def GraphBFS(problem):
+    visited = 0
+    expanded_nodes = 0
+    maximum_memory = 0
+
     node = Node(problem.initial_state,None,None)
     print(node)
     if problem.goal_test(node.state):
-        return node
+        return (node, (visited,expanded_nodes,maximum_memory,node.cost))
     frontier = deque([node])
     explored = set()
     while True:
@@ -20,22 +24,27 @@ def GraphBFS(problem):
            return None
        node = frontier.pop()
        explored.add(node.state)
+       visited += 1
+
        for action in problem.actions(node.state):
-           child = child_node(problem, node, action)
+           child = node.child_node(problem, action, node.cost + 1)
            if (child.state not in explored):
                if(child not in frontier):
                    print(child.state + " <- " + child.parent.state)
                    if problem.goal_test(child.state):
-                       return child
+                       return (child, (visited,expanded_nodes,maximum_memory,child.cost))
                    frontier.appendleft(child)
+                   expanded_nodes += 1
+                   maximum_memory = max(maximum_memory,len(frontier) + len(explored))
 
 
 def main():
     romania_graph = pg.get_romania_graph()
     romania_prob = RomaniaProblem("Arad","Bucharest", romania_graph)
-    node = GraphBFS(romania_prob)
+    node, details = GraphBFS(romania_prob)
     if node is not None:
         node.print_path()
+        print(details)
     return
 if __name__ == "__main__":
         main()
--- tree_bfs.py
+++ tree_bfs.py
@@ -9,29 +9,37 @@ def child_node(problem, node, action):
 
 
 def TreeBFS(problem):
+    visited = 0
+    expanded_nodes = 0
+    maximum_memory = 0
+
     node = Node(problem.initial_state,None,None)
     if problem.goal_test(node.state):
-        return node
+        return (node, (visited,expanded_nodes,maximum_memory,node.cost))
     frontier = deque([node])
     while True:
        if len(frontier) == 0:
-           return None
+           return (None,None)
        node = frontier.pop()
+       visited += 1
        for action in problem.actions(node.state):
-           child = node.child_node(problem, action)
+           child = node.child_node(problem, action,node.cost + 1)
            if(child.state not in frontier):
                    print(child.state + " <- " + child.parent.state)
                    if problem.goal_test(child.state):
-                       return child
+                       return (child, (visited,expanded_nodes,maximum_memory,child.cost))
                    frontier.appendleft(child)
+                   expanded_nodes += 1
+                   maximum_memory = max(maximum_memory,len(frontier))
 
 
 def main():
     romania_graph = pg.get_romania_graph()
     romania_prob = RomaniaProblem("Arad","Bucharest", romania_graph)
-    node = TreeBFS(romania_prob)
+    node, details = TreeBFS(romania_prob)
     if node is not None:
         node.print_path()
+        print(details)
     return
 
 if __name__ == "__main__":
